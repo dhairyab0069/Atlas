b92fda28f8d951e55cd1e8ba97b07df209089ae9
MehulRaisingh <mehul.raisingh@gmail.com>
2022-11-30 18:18:30 -0800

    adding Journal for each exercise

--- app/src/main/java/com/example/atlas/GymJournal.java
+++ app/src/main/java/com/example/atlas/GymJournal.java
@@ -2,13 +2,139 @@ package com.example.atlas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class GymJournal extends AppCompatActivity {
 
+    Button submit, home  ;
+    RadioGroup rgroup;
+    RadioButton rbutton;
+
+    String exerciseChoice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym_journal);
+
+        rgroup = (RadioGroup) findViewById(R.id.exradiogroup);
+        submit = (Button) findViewById(R.id.SelEx) ;
+        home = (Button) findViewById(R.id.ExJournalHome) ;
+
+
+submit.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+
+        int radioid = rgroup.getCheckedRadioButtonId();
+
+
+
+        if (radioid == -1) {
+
+
+            Toast.makeText(GymJournal.this, "Select an exercise", Toast.LENGTH_SHORT).show();
+        } else {
+
+
+            rbutton = findViewById(radioid);
+            exerciseChoice = rbutton.getText().toString();
+
+
+            switch (exerciseChoice){
+
+                case "Bench Press":
+                    benchJournal();
+                    break;
+
+                case "Squat":
+                    squatJournal();
+                    break;
+
+                case "Deadlift":
+                    deadliftJournal();
+                    break;
+
+                case "Pull ups":
+                    pullupJournal();
+                    break;
+
+                default:
+                    break;
+
+            }
+
+        }
+
+
+
+
+
+
     }
+});
+
+
+home.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        home();
+    }
+});
+
+
+    }
+
+    public void benchJournal(){ //creates new intent
+
+        Intent intent = new Intent(this,benchJournal.class);
+        startActivity(intent);
+        finish();
+
+
+
+    }
+
+    public void squatJournal(){ //creates new intent
+
+        Intent intent = new Intent(this,squatJournal.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    public void deadliftJournal(){ //creates new intent
+
+        Intent intent = new Intent(this,deadliftJournal.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    public void pullupJournal(){ //creates new intent
+
+        Intent intent = new Intent(this,pullupJournal.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    public void home(){ //creates new intent
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/atlas/pullupJournal.java
@@ -0,0 +1,14 @@
+package com.example.atlas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pullupJournal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pullup_journal);
+    }
+}
