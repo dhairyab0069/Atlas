8fd69029ae664e0a0bdbce3f7ad9ae1a70d777fb
dhairyab0069 <bhatiadhairya19@gmail.com>
2022-12-04 23:28:06 -0800

    Done metrics

---
+++ app/src/main/java/com/example/atlas/RemoveClickListener.java
@@ -0,0 +1,7 @@
+package com.example.atlas;
+
+import java.util.ArrayList;
+
+public interface RemoveClickListener {
+    void OnRemoveClick(int index, ArrayList<RecyclerViewData> myList,String value);
+}
