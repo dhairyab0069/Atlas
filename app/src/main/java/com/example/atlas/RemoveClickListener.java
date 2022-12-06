1455982d88146e78f880b5915b8d2d01fc2dda55
Caps1d <ye.smertenko@gmail.com>
2022-12-05 19:53:19 -0800

    Removing error code

--- app/src/main/java/com/example/atlas/RemoveClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.atlas;
-
-import java.util.ArrayList;
-
-public interface RemoveClickListener {
-    void OnRemoveClick(int index, ArrayList<RecyclerViewData> myList,String value);
-}
